b86bcf14b19fcd8b85b966e2467f5e22b5896de8
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-01-14 13:12:46 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		sort(a.begin(), a.end());
+		// simply get the difference between the minimum and maximum element
+		cout << a[n - 1] - a[0] << '\n';
+  }
+	return 0;
+}
